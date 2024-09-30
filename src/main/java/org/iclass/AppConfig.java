4911c119df9ca2c1de5243fb682ebc7ca2cf5e67
jyjy <jungjuneyoung6@gmail.com>
2024-09-30 14:22:29 +0900

    branchA

--- src/main/java/org/iclass/AppConfig.java
+++ src/main/java/org/iclass/AppConfig.java
@@ -7,17 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-    public String URL_PATH = "/upload/**";
-    // 파일시스템 위치
-    public String LOCATION ="file:///C:/upload/";
+    private String urlPath = "/upload/**";
+    private String location = "file:///C:/upload/";
+
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-
-        //위의 2개 값을 매핑
-        registry.addResourceHandler(URL_PATH)
-                .addResourceLocations(LOCATION);
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
+        resourceHandlerRegistry.addResourceHandler(urlPath)
+                .addResourceLocations(location);
 
     }
 
